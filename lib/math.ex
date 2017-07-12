6bd3b4268d6850846e7bfd8e12624a3b4471b52f
Ryo Atsuta <blackstickjavadev@gmail.com>
2017-07-12 20:26:59 +0900

    Commends have been removed for now

--- lib/math.ex
+++ lib/math.ex
@@ -1,18 +1,4 @@
 defmodule Elixir101.Math do
-  @moduledoc """
-  Documentation for Elixir101.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Elixir101.hello
-      :world
-
-  """
-
   def sum(numbers) when is_map(numbers), do: Enum.to_list(numbers) |> sum
   def sum([]), do: 0
   def sum([number | numbers]) when is_number(number) do
