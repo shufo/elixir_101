f493444839b6db29c6cb1043c186d13ab0897098
Ryo Atsuta <blackstickjavadev@gmail.com>
2017-07-13 12:32:56 +0900

    Code Refactoring

--- lib/util.ex
+++ lib/util.ex
@@ -1,35 +1,14 @@
 defmodule Elixir101.Util do
-  def multiply(numbers, factor) when is_number(factor) do
-    if is_binary(numbers) do
-      raise StringCannotMultiplyError
-    end
+  def multiply(numbers, _factor) when is_binary(numbers), do: raise StringCannotMultiplyError
+  def multiply(_numbers, factor) when factor < 0, do: raise "factor #{factor} is negative"
+  def multiply(numbers, factor) when is_number(factor), do: numbers |> Enum.map(&(&1 * factor))
 
-    # raise RuntimeError if factor is negative
-    if factor < 0 do
-      raise "factor #{factor} is negative"
-    end
-    numbers |> Enum.map(&(&1 * factor))
-  end
-
-  def split(words_string) when is_binary(words_string) do
-    ~w(#{words_string})
-  end
+  def split(words_string) when is_binary(words_string), do: ~w(#{words_string})
 
   def prev_num([], _value), do: nil
   def prev_num([_current], _value), do: nil
-  def prev_num([prev, current], value) do
-    if current === value do
-      prev
-    else
-      prev_num([], value)
-    end
-  end
-  def prev_num([prev|list], value) when is_list(list) do
-    current = hd list
-    if current === value do
-      prev
-    else
-      prev_num(list, value)
-    end
-  end
+  def prev_num([prev, current], value) when current === value, do: prev
+  def prev_num([_prev, _current], value), do: prev_num([], value)
+  def prev_num([prev|[current|_]], value) when current === value, do: prev
+  def prev_num([_prev|list], value), do: prev_num(list, value)
 end
