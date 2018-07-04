c8cdd479f47de3f753a94af79e6a5314d67792b8
kooo5252 <masaki.affiliate@gmail.com>
2018-07-04 17:30:02 +0900

    Update elixir_101.ex

--- lib/elixir_101.ex
+++ lib/elixir_101.ex
@@ -1,18 +1,64 @@
 defmodule Elixir101 do
-  @moduledoc """
-  Documentation for Elixir101.
-  """
 
-  @doc """
-  Hello world.
+  defmodule Util do
+ 
+    def split(input) do
+      input |> String.split
+    end
 
-  ## Examples
+    defmodule MyError do
+      defexception message: "StringCannotMultiplyError"
+    end
 
-      iex> Elixir101.hello
-      :world
+    def multiply(input, multiple) do
 
-  """
-  def hello do
-    :world
+      try do
+        input |> Enum.map(fn(x) -> x * multiple end)
+      rescue
+        e in RuntimeError -> IO.puts("RuntimeError")
+        e in Protocol.UndefinedError -> IO.puts("StringCannotMultiplyError")
+      end
+
+    end
+
+    def prev_num(input, post_val) do
+      if Enum.member?(input, post_val ) == true,
+        do: input |> Enum.at( Enum.find_index(input, fn(x) -> x==post_val end) - 1),
+        else: nil
+    end
+
   end
+
+#--------------------------------------
+
+  defmodule Math do
+  
+    def sum(input) do
+      input |> Enum.sum
+    end
+
+  end
+
+#--------------------------------------
+
+  defmodule Job do
+  
+    def async(input) do
+      input.()
+    end
+
+  end
+
+#--------------------------------------
+
+  defmodule API do
+  
+    def request(input) do
+      input |> URI.parse |> Map.get(:query)
+    end
+
+  end
+
+#--------------------------------------
+
 end
